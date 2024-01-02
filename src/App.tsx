bfc3824fe0486ed3782cbd085a43d3e284640aa8
arkh1 <arkh1@student.le.ac.uk>
2024-01-02 18:26:31 +0000

    Start up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,32 @@ function App() {
   return (
     <>
       <div>
+
+        <header>
+        <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
+        </header>
+
+        <main>
+          <section>
+            <h2>Voter Registration</h2>
+            <p>Register as a voter to participate in the election.</p>
+            <a href=''>Register Now</a>
+          </section>
+
+          <section>
+            <h2>VoterLogin</h2>
+            <p>Login to make your vote.</p>
+            <a href=''>Login Now</a>
+          </section>
+
+          <section>
+            <h2>Election Commission Officer Login</h2>
+            <p>Login as an Election Commission Officer to manage the election.</p>
+            <a href=''></a>
+          </section>
+        </main>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +44,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite  React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,5 +59,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
